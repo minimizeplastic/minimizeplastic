86b474152614928891f6a626616535f96efe83c1
niloofarmotamed <niloofar.motamed@gmail.com>
2018-08-19 07:48:02 +0300

    Put data in a seperate file

---
+++ src/data.js
@@ -0,0 +1,218 @@
+export const cards = [
+    {
+      "brand": "GoGreen",
+      "productFamily": "Pavut",
+      "productName": "Suuret valkoiset luomupavut",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-stora-vita-bonor-285g-2.png",
+      "id": "da3cd200-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Pavut",
+      "productName": "Luomu kidneypavut",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-kidneybonor-285g-ny.png",
+      "id": "da3cd201-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Pavut",
+      "productName": "Luomu papumix",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-bonmix-285g-ny.png",
+      "id": "da3cd202-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Pavut",
+      "productName": "Mustapavut",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fsvarta-bonor-285g-ny2.png",
+      "id": "da3cd203-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Pavut",
+      "productName": "Mungpavut (kuivat)",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fmungbonor-ny.png",
+      "id": "da3cd204-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Pavut",
+      "productName": "Ruskeita papuja",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fbruna-bonor-ny.png",
+      "id": "da3cd205-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Linssit",
+      "productName": "Vihreät linssit",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fgrona-linser-285g-ny2.png",
+      "id": "da3cd206-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Linssit",
+      "productName": "Vihreät linssit (kuivat)",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fgrona-linser-ny.png",
+      "id": "da3cd207-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Linssit",
+      "productName": "Punaiset linssit",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2froda-linser-290g-ny2.png",
+      "id": "da3cd208-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Herneet",
+      "productName": "Luomu kikherneet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-kikartor-290g-ny.png",
+      "id": "da3cd209-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Herneet",
+      "productName": "Vihreät luomuherneet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-grona-arter-ny.png",
+      "id": "da3cd20a-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Herneet",
+      "productName": "Kikherneet (kuivat)",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fkikarter-ny.png",
+      "id": "da3cd20b-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Siemenet",
+      "productName": "Auringonkukan siemenet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fsolroskarnor-ny.png",
+      "id": "da3cd20c-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Siemenet",
+      "productName": "Pellavansiemenet (kokonaisina)",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2flinfro-ny.png",
+      "id": "da3cd20d-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Siemenet",
+      "productName": "Kuorettomat seesaminsiemenet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fskalat-sesamfro-ny.png",
+      "id": "da3cd20e-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Siemenet",
+      "productName": "Kuorimattomat seesaminsiemenet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2foskalat-sesamfro-ny.png",
+      "id": "da3cd20f-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Siemenet",
+      "productName": "Rouhitut pellavansiemenet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fkrossat-linsfro-260g-ny.png",
+      "id": "da3cd210-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Vihannekset",
+      "productName": "Vihreät luomuherneet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-grona-arter-ny.png",
+      "id": "da3cd211-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Vihannekset",
+      "productName": "Luomu tomaattimurska",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-krossade-tomater-300g-ny.png",
+      "id": "da3cd212-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Vihannekset",
+      "productName": "Luomumaissi",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2feko-majskorn-280g-ny.png",
+      "id": "da3cd213-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Salsakastikkeet",
+      "productName": "Salsa Original",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fprodukt%2fsalsa-original-ny.png",
+      "id": "da3cd214-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Salsakastikkeet",
+      "productName": "Chili sin Carne",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fprodukt%2fchili-sin-carne-ny.png",
+      "id": "da3cd215-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Ruokajyvät ja suurimot",
+      "productName": "Bulgur-vehnää",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fbulgur-ny.png",
+      "id": "da3cd216-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Ruokajyvät ja suurimot",
+      "productName": "Couscous (täysjyvä)",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2ffullkorns-couscous-ny.png",
+      "id": "da3cd217-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Ruokajyvät ja suurimot",
+      "productName": "Kokonaisia tattarisuurimoita",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fhelt-bovete-ny.png",
+      "id": "da3cd218-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Ruokajyvät ja suurimot",
+      "productName": "Kvinoa",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fquinoa-ny.png",
+      "id": "da3cd219-a36a-11e8-8b8b-73731145bce2"
+    },
+    {
+      "brand": "GoGreen",
+      "productFamily": "Ruokajyvät ja suurimot",
+      "productName": "Ohralisukkeet",
+      "shops": "S-market, K-market",
+      "img": "http://www.gogreen.fi/Handlers/TransformedImage.ashx?w=377&h=406&url=%2fGlobal%2fprodukter%2fkorngryn-ny.png",
+      "id": "da3cd21a-a36a-11e8-8b8b-73731145bce2"
+    }
+  ];
