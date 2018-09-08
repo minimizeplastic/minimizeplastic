6cd934b7c1f3c5333cbffbc9093ce43953cf98e3
Daniel Schildt <daniel.schildt@gmail.com>
2018-09-08 22:26:49 +0300

    Improve test structure
    - Improve test structure
    - Start using Enzyme for shallow rendering
    - Start using Jest Snapshot Testing feature

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'
 import App from '.';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  describe('with ReactDOM', () => {
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  });
+
+  describe('with Enzyme', () => {
+    it('shallow renders without crashing', () => {
+      shallow(<App />);
+    });
+  });
+
+  describe('with Snapshot Testing', () => {
+    it('renders the same way', () => {
+      const component = renderer.create(<App />)
+      const tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+    });
+  });
 });
